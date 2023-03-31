a454466d7fa4674caad87fc202edd9d0ddaa30a8
Mark Roddy <markroddy@gmail.com>
2023-03-31 19:04:03 +0000

    Pull out scalar udf out into its own file to make room for table function too

---
+++ src/include/pyscalar.hpp
@@ -0,0 +1,8 @@
+
+#include "duckdb.hpp"
+
+namespace pyudf {
+  std::string executePythonFunction(const std::string &module_name, const std::string &function_name,
+                                    const std::string &argument);
+
+}
