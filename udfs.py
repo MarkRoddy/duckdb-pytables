9704fe03a651a3af36f6d52f7bb941a345b9a19b
Mark Roddy <markroddy@gmail.com>
2023-04-05 10:24:04 -0700

    Arbitrary Table Function Arguments (#10)
    * Table function now accepts arbitrary arguments to be sent to the underlying python function.
    
    * make format

--- udfs.py
+++ udfs.py
@@ -3,7 +3,13 @@
 def reverse(input):
     return input[::-1]
 
-
 def table(input):
     for char in "a very long string":
         yield [char]
+
+def table2(one_str_input, two_str_input, three_int_input):
+    for c in one_str_input:
+        yield [c]
+    for c in two_str_input:
+        yield [c]
+    yield [str(three_int_input)]
