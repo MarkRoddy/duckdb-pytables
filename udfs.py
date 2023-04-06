2845faf2d0bc549ac95c96b287fa184de219c6aa
Mark Roddy <markroddy@gmail.com>
2023-04-06 23:51:17 +0000

    Also add some tests for the udfs

--- udfs.py
+++ udfs.py
@@ -28,3 +28,39 @@ def iterator_throws_exception(input):
     yield ["bar"]
     raise Exception("Third record raises an exception")
 
+import unittest
+
+class TestUdfs(unittest.TestCase):
+
+    def test_reverse(self):
+        self.assertEquals("raboof", reverse("foobar"))
+
+    def test_table(self):
+        actual = [record[0] for record in table("")]
+        expected = ["a", " ", "v", "e", "r", "y", " ", "l", "o", "n", "g", " ", "s", "t", "r", "i", "n", "g"]
+        self.assertEquals(actual, expected)
+
+    def test_table2(self):
+        actual = [record[0] for record in table2('foo', 'bar', 16)]
+        expected = ['f', 'o', 'o', 'b', 'a', 'r', '16']
+        self.assertEquals(actual, expected)
+
+    def test_table_throws_exception(self):
+        try:
+            table_throws_exception("")
+        except Exception as e:
+            self.assertEquals("This function raises an exception", str(e))
+
+    def test_iterator_throws_exception(self):
+        iterator = iterator_throws_exception("")
+        first = next(iterator)
+        self.assertEquals(first, ["foo"])
+        second = next(iterator)
+        self.assertEquals(second, ["bar"])
+        try:
+            next(iterator)
+        except Exception as e:
+            self.assertEquals("Third record raises an exception", str(e))
+                          
+if __name__ == '__main__':
+    unittest.main()
