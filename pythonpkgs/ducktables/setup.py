9818100a8f25e263442bd57c55af5edbb62a0505
Mark Roddy <markroddy@gmail.com>
2023-05-19 19:36:06 +0000

    Bump version number for release

--- pythonpkgs/ducktables/setup.py
+++ pythonpkgs/ducktables/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ducktables',
-    version='0.1.0',
+    version='0.1.1',
     description='Python Table Functions for DuckDB',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
