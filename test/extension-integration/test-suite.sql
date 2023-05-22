47763e32fb9ac1a00c3a66adcb4b98a9dd9769fe
Mark Roddy <markroddy@gmail.com>
2023-05-22 22:31:45 +0000

    clean up sql script

--- test/extension-integration/test-suite.sql
+++ test/extension-integration/test-suite.sql
@@ -1,15 +1,8 @@
 
 load python_udf;
 
--- SELECT *
--- FROM pytable('ducktables.google:sheet', '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms', 'Class Data!A2:F31',
---      columns = {
---              'name': 'VARCHAR', 'gender': 'VARCHAR', 'class_level': 'VARCHAR',
---              'state': 'VARCHAR', 'major': 'VARCHAR', 'extracurricular': 'VARCHAR'}
---     )
--- ORDER BY name ASC
--- LIMIT 2;
-
+-- Note we don't actually assert the contents of the result. The main thing is to
+-- ensure we can exercise DuckDB -> UDF Extension -> Python -> Remote System and back
 SELECT *
 FROM pytable('ducktables.github:repos_for', 'markroddy',
      columns = {'repo': 'VARCHAR', 'description': 'VARCHAR', 'language': 'VARCHAR'})
