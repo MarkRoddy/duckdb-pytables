4f3ce3886365cd83273544e7b27ea44e89bf39a8
Mark Roddy <markroddy@gmail.com>
2023-05-26 22:16:43 +0000

    Python package wasn't package-ified

--- pythonpkgs/ducktables/setup.py
+++ pythonpkgs/ducktables/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ducktables',
-    version='0.1.1',
+    version='0.1.2',
     description='Python Table Functions for DuckDB',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
