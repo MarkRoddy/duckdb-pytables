b4551f6d1d02061ab631ab5556dfc8b458fd3bf6
Mark Roddy <markroddy@gmail.com>
2023-06-02 22:23:01 +0000

    Remove install step from testing script which shouldn't be necessary since we're using the installer to install the extension

--- test/post-release-integration/test-query.sql
+++ test/post-release-integration/test-query.sql
@@ -1,10 +1,6 @@
 
 
-
-SET custom_extension_repository='net.ednit.duckdb-extensions.s3.us-west-2.amazonaws.com/pytables/${RELEASE_SHA}/python${PYTHON_VERSION}';
-INSTALL pytables;
 LOAD pytables;
-
 
 -- Note we don't actually assert the contents of the result. The main thing is to
 -- ensure we can exercise DuckDB -> UDF Extension -> Python -> Remote System and back
