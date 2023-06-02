72564cf992a1ff2f78bdbcaf726391f367a396be
Mark Roddy <markroddy@gmail.com>
2023-06-02 19:51:07 +0000

    Sorted through a whole host of issues

--- installer/pytables-install.py
+++ installer/pytables-install.py
@@ -1,15 +1,31 @@
 """Installs the Extension"""
 
 import os, sys
+import time
 import shutil
 import subprocess
 import ctypes.util
 import subprocess
+import logging as log
 
 def get_python_version():
     return f"{sys.version_info.major}.{sys.version_info.minor}"
 
 def find_libpython():
+    log.debug("Looking for libpython...")
+    lib = find_libpython_ctypes()
+    if lib:
+        log.debug("Found via ctypes: " + lib)
+        return lib
+    else:
+        log.debug("Nothing found via ctypes, falling back to ldconfig")
+        try:
+            return find_libpython_ldconfig()
+        except Exception as e:
+            log.debug("Error using ldconfig: " + str(e))
+            return None
+
+def find_libpython_ctypes():
     python_version = f'{sys.version_info.major}.{sys.version_info.minor}'
 
     # Try different names for the shared library
@@ -19,12 +35,35 @@ def find_libpython():
         f'libpython{python_version}.so',
         f'libpython{python_version}m.so',
         ]
-
+    
     for library_name in library_names:
         library_path = ctypes.util.find_library(library_name)
         if library_path is not None:
             return library_path    
     return None
+
+def find_libpython_ldconfig():
+    python_version = f'{sys.version_info.major}.{sys.version_info.minor}'
+    library_names = [
+        f'libpython{python_version}.so',
+        f'libpython{python_version}m.so',
+        ]
+
+    # Get the output of `ldconfig -p`
+    output = subprocess.check_output(['ldconfig', '-p']).decode()
+
+    # Go through each line in the output
+    lines = output.split('\n')
+    log.debug("Checking %s libraries from ldconfig" % len(lines))
+    for line in lines:
+        line = line.strip()
+        for target in library_names:
+            if target in line:
+                log.debug("Found %s in %s" %(target, line))
+                return line.split()[-1]
+    log.debug("Did not find a libpython in ldconfig output")
+    return None
+                                                                                        
 
 def check_pip():
     try:
@@ -56,16 +95,22 @@ def get_libpython_apt_install_cmd():
 def get_libpython_install_cmd():
     if is_apt_based():
         return get_libpython_apt_install_cmd()
-
+    else:
+        log.debug("OS is not apt based, don't know how to suggest installing")
 
 def find_duckdb():
     ddb = shutil.which('duckdb')
-    if not ddb:
+    if ddb:
+        log.debug("Found duckdb via shutil.which()")
+        return ddb
+    else:
         # If it isn't in our path, make sure it wasn't curl'd into our CWD.
         possible_path = os.path.join(os.getcwd(), 'duckdb')
         if os.path.isfile(possible_path) and os.access(possible_path, os.X_OK):
-            ddb = possible_path
-    return ddb
+            log.debug("Found in cwd")
+            return possible_path
+    log.debug("No DuckDB binary found")
+    return None
 
 def generate_install_sql():
     python_version = get_python_version()
@@ -82,22 +127,27 @@ def run_query(ddb, query):
     return process.returncode
                 
 def main(args):
-    libpython = find_libpython()
-    if not libpython:
-        install_cmd = get_libpython_install_cmd()
-
-        if install_cmd:
-            print(f"Unable to find the needed python{get_python_version()}.so shared library. If you installed Python via your package manager, you can install the shared library with the following command:")
-            print(install_cmd)
-        else:
-            print(f"Unable to find the needed python{get_python_version()}.so shared library. If you installed Python via a package, check if there is an additional libpython package.")
-            print("Alternatively, if you've compiled your Python interpretter from source, make sure you specify the shared library is installed by running `./configure --enable-shared` before compiling.")
-        return 1
+    log.basicConfig(filename='/tmp/' + args[0] + '.log', filemode='w',
+                    level=log.DEBUG,
+                    format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    log.Formatter.converter = time.gmtime
     ddb_path = find_duckdb()
     if not ddb_path:
         print("Unable to find a DuckDB binary. If you've already downloaded it, did you place it in your $PATH?")
         print("If you havent' downloaded it, grab a copy here: https://duckdb.org/docs/installation/")
         return 1
+
+    libpython = find_libpython()
+    if not libpython:
+        install_cmd = get_libpython_install_cmd()
+
+        if install_cmd:
+            print(f"Unable to find the needed python{get_python_version()}.so shared library, though this man not necessarily mean there is a problem. If you see a simliar error when you load the pytables extension, you'll need to install this library. If you installed Python via your package manager, you can install the shared library with the following command:")
+            print(install_cmd)
+        else:
+            print(f"Unable to find the needed python{get_python_version()}.so shared library, thought his may not necessarily mean there is a problem. If you see a simliar error when you load the pytables extension, you'll need to install this library. If you installed Python via a package, check if there is an additional libpython package.")
+            print("Alternatively, if you've compiled your Python interpretter from source, make sure you specify the shared library is installed by running `./configure --enable-shared` before compiling.")
+
     ddb = [ddb_path, '-unsigned']
     query = generate_install_sql()
     result = run_query(ddb, query)
--- installer/test-installer.sh
+++ installer/test-installer.sh
@@ -12,31 +12,36 @@ PLATFORM="$2"
 PYTHON_VERSION="$3";
 DUCKDB_VERSION="$4";
 
-# This should fail because of a missing libpython
-python${PYTHON_VERSION} /pytables-install.py | tee output.txt
-EC="${PIPESTATUS[0]}";
-if [ 0 -eq "$EC" ]; then
-    echo "Installer did not exit on missing libpython was missing as expected"
-    exit 1
-elif ! grep -q libpython output.txt; then
-    echo "Did not find reference to libpython in output as expected";
+function exit-error () {
+    if [ -f /tmp/pytables-install.py.log ]; then
+        cat /tmp/pytables-install.py.log;
+    fi
+    if [ -n "$1" ]; then
+        status "$1";
+    fi
+    exit 1;
+}
+
+function status () {
+    echo -n "Test Status Message: " 1>&2;
+    echo "$1" 1>&2;
+}
+
+if [ -z "$GITHUB_ACCESS_TOKEN" ]; then
+    exit-error "Missing GITHUB_ACCESS_TOKEN needed to run sql script"
 fi
-
-# Install the libpython
-apt-get install -y -qq libpython${PYTHON_VERSION}
-
 
 # This should fail because of missing DuckDB
 python${PYTHON_VERSION} /pytables-install.py | tee output.txt
 EC="${PIPESTATUS[0]}";
 if [ $EC -eq 0 ]; then
-    echo "Installer did not exit on missing duckdb"
-    exit 1
-elif ! grep -q 'duckdb.*PATH' output.txt; then
-    echo "Did not find reference to missing DuckDB in output as expected";
+    exit-error "Installer did not exit on missing duckdb"
 fi
-
-
+if grep -nq 'duckdb' output.txt; then
+    status "Found reference to missing duckdb as expected";
+else
+    exit-error "Did not find reference to missing DuckDB in output as expected";
+fi
 
 test -n "$DUCKDB_VERSION" 
 curl -Lo /tmp/duckdb_cli.zip "https://github.com/duckdb/duckdb/releases/download/v${DUCKDB_VERSION}/duckdb_cli-${OS}-${PLATFORM}.zip" && \
@@ -44,21 +49,65 @@ curl -Lo /tmp/duckdb_cli.zip "https://github.com/duckdb/duckdb/releases/download
     mv /tmp/duckdb /usr/bin/duckdb && \
     rm /tmp/duckdb_cli.zip
 
+
+# Installation should now fail because of the missing shared library. Our script will issue
+# a warning and then try to install the extension which will fail.
+# Note we don't prevent the install as finding libpython is something that is fraught with false
+# negatives (detecting it's missing when it is present). So we confirm the warning but don't
+# expect the process to fail.
+python${PYTHON_VERSION} /pytables-install.py | tee output.txt
+if grep -q libpython output.txt; then
+    status "Found reference to libpython as expected";
+else
+    exit-error "Did not find reference to libpython in output as expected";
+fi
+
+# Install the libpython
+apt-get install -y -qq libpython${PYTHON_VERSION}
+
+# Installation should now work but our warning is still present as script is unable to find it
+python${PYTHON_VERSION} /pytables-install.py | tee output.txt
+if grep -q libpython output.txt; then
+    status "Found reference to libpython as expected";
+else
+    exit-error "Did not find reference to libpython in output as expected";
+fi
+
+# Update the shared library cache so our script will be able to find it
+ldconfig
+
+# Now that we've installed the library + updated ldconfig cache, we should not have a warning.
+python${PYTHON_VERSION} /pytables-install.py | tee output.txt
+if grep -q libpython output.txt; then
+    exit-error "Found reference to libpython that we did not expect because we've updated ldconfig cache";
+else
+    status "No reference to libpython as expected"
+fi
+
 # Extension installation should work now, but the Python package install will not.
 python${PYTHON_VERSION} /pytables-install.py
-cat test-load-extension.sql | duckdb -unsigned | tee output.txt
-EC="${PIPESTATUS[1]}";
-if [ $EC -nq 0 ]; then
-    echo "DuckDB did not find the extension as expected"
-    exit 1
+
+# Confirm output regarding pip being missing
+if grep -q "No 'pip' module found." output.txt; then
+    status "Found expected missing pip error message";
+else
+    exit-error "Did not find expected missing pip message";
 fi
 python${PYTHON_VERSION} -c "import ducktables" | tee output.txt
 EC="${PIPESTATUS[0]}";
 if [ $EC -eq 0 ]; then
-    echo "We were able to install pypackage but don't expect to be able to."
-    echo "This is most likely a bug in the test setup where we are some how"
-    echo "not preventing the package install as expected/desired"
-    exit 1
+    status "We were able to install pypackage but don't expect to be able to."
+    status "This is most likely a bug in the test setup where we are some how"
+    status "not preventing the package install as expected/desired"
+    exit-error
+fi
+
+# Even though we were not able to install the Python package, the extension should
+# be installed. Confirm this is the case.
+cat test-load-extension.sql | duckdb -unsigned | tee output.txt
+EC="${PIPESTATUS[1]}";
+if [ $EC -ne 0 ]; then
+    exit-error "DuckDB could not load the extension as expected"
 fi
 
 
@@ -68,8 +117,8 @@ apt-get install -y -qq python${PYTHON_VERSION}-distutils && \
     curl https://bootstrap.pypa.io/get-pip.py | python${PYTHON_VERSION}
 
 # Re-run the installer, python package should be installed this time.
-python${PYTHON_VERSION} /tmp/pytables-install.py && python${PYTHON_VERSION} -c "import ducktables"
+python${PYTHON_VERSION} /pytables-install.py && python${PYTHON_VERSION} -c "import ducktables"
 
 # Confirm by trying to query a ducktables function
-RUN cat /test-query.sql | duckdb -unsigned 
+cat /test-run-query.sql | duckdb -unsigned 
 
