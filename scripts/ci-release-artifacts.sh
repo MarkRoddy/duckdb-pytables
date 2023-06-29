7aa492c4cd73a10ac36bf1cfe6e8085346af59ab
Mark Roddy <markroddy@gmail.com>
2023-06-29 18:54:50 +0000

    Remove Actions references from release script

--- scripts/ci-release-artifacts.sh
+++ scripts/ci-release-artifacts.sh
@@ -7,6 +7,20 @@ set -e;
 # logic for the step that Github Actions performs. Feel free to merge the two if you ever
 # feel like it.
 
+if [ -z "$ARCH" ]; then
+    echo "Missing required env var ARCH";
+    exit 1;
+fi
+if [ -z "$PYTHON_VERSION" ]; then
+    echo "Missing required env var PYTHON_VERSION";
+    exit 1;
+fi
+if [ -z "$GITHUB_REF_NAME" ]; then
+    echo "Missing required env var GITHUB_REF_NAME";
+    exit 1;
+fi
+
+
 git config --global --add safe.directory '*'
 cd duckdb
 git fetch --tags
@@ -18,11 +32,12 @@ if [[ "$AWS_ACCESS_KEY_ID" == "" ]] ; then
 elif [[ "$GITHUB_REF" =~ ^(refs/tags/v.+)$ ]] ; then
     echo "Uploading release tagged $GITHUB_REF";
     python3 -m pip install pip awscli
-    ./scripts/extension-upload.sh pytables ${{ github.ref_name }} $DUCKDB_VERSION ${{matrix.arch}} $BUCKET_NAME true python${{ matrix.python_version }}
+    # ./scripts/extension-upload.sh pytables ${{ github.ref_name }} $DUCKDB_VERSION $ARCH $BUCKET_NAME true python${PYTHON_VERSION}
+    ./scripts/extension-upload.sh pytables $GITHUB_REF_NAME $DUCKDB_VERSION $ARCH $BUCKET_NAME true python${PYTHON_VERSION}
 elif [[ "$GITHUB_REF" =~ ^(refs/heads/main)$ ]] ; then
     echo "Uploading tip of main branch $GITHUB_REF";
     python3 -m pip install pip awscli
-    ./scripts/extension-upload.sh pytables `git log -1 --format=%h` $DUCKDB_VERSION ${{matrix.arch}} $BUCKET_NAME false python${{ matrix.python_version }}
+    ./scripts/extension-upload.sh pytables `git log -1 --format=%h` $DUCKDB_VERSION $ARCH $BUCKET_NAME false python${PYTHON_VERSION}
 else
     echo "Build is for non tag on a branch other than main, skipping"
 fi
